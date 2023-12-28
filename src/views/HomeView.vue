62f03002d6b07e92d8588ce26eb04af281825ae3
Krittapas Rungsimontuchat <nineten609@gmail.com>
2023-12-28 21:08:10 +0700

    feat:navbar

---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<script setup>
+import UserLayout from "@/layouts/UserLayout.vue";
+</script>
+
+<template>
+  <UserLayout>
+    <button class="btn">Button</button>
+    <button class="btn btn-neutral">Neutral</button>
+    <button class="btn btn-primary">Primary</button>
+    <button class="btn btn-secondary">Secondary</button>
+    <button class="btn btn-accent">Accent</button>
+    <button class="btn btn-ghost">Ghost</button>
+    <button class="btn btn-link">Link</button>
+  </UserLayout>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+
+  // ... other configurations
+  plugins: [
+    require("daisyui"),
+    // ... other plugins
+  ],
+  daisyui: {
+    themes: ["cupcake"],
+  },
+};
